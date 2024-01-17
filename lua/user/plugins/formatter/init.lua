b70492d9d5f55e76f4252a4143ceefa4b991b9de
nishantHolla <nishantholla19@gmail.com>
2024-01-17 21:37:25 +0530

    Update formatters

--- lua/user/plugins/formatter/init.lua
+++ lua/user/plugins/formatter/init.lua
@@ -1,29 +1,33 @@
 local util = require('formatter.util')
 
+local formatters = {
+  lua = { name = 'stylua' },
+  python = { name = 'black' },
+  c = { name = 'clangformat' },
+  css = { name = 'prettier' },
+  sass = { ft = 'css', name = 'prettier' },
+  scss = { ft = 'css', name = 'prettier' },
+  cpp = { name = 'clangformat' },
+  javascript = { name = 'prettier' },
+  javascriptreact = { name = 'prettier' },
+  typescript = { name = 'prettier' },
+  typescriptreact = { name = 'prettier' },
+  html = { name = 'prettier' },
+}
+
+local config = {}
+
+for filetype, formatter in pairs(formatters) do
+  local file = formatter.ft or filetype
+  config[filetype] = { require('formatter.filetypes.' .. file)[formatter.name] }
+end
+
+config['*'] = {
+  require('formatter.filetypes.any').remove_trailing_whitespace,
+}
+
 require('formatter').setup({
   logging = true,
   log_level = vim.log.levels.WARN,
-  filetype = {
-    lua = {
-      require('formatter.filetypes.lua').stylua,
-
-      function()
-        return {
-          exe = 'stylua',
-          args = {
-            '--search-parent-directories',
-            '--stdin-filepath',
-            util.escape_path(util.get_current_buffer_file_path()),
-            '--',
-            '-',
-          },
-          stdin = true,
-        }
-      end,
-    },
-
-    ['*'] = {
-      require('formatter.filetypes.any').remove_trailing_whitespace,
-    },
-  },
+  filetype = config,
 })
